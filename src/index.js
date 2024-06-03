635cb95faed4c0f380589df8aa7df5a80e1d5a2d
Yacith-D <martinezyacith@gmail.com>
2024-06-03 15:51:20 -0600

    funcion que hace que muestre texto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-// Inserte el código aquí
+let btn = document.getElementById("btnStyle")
+let contieneHijo = document.getElementById("container")
+let inputMostrar = document.getElementById("barra")
+  
+
+btn.addEventListener("click", function () {
+    let crearDiv = document.createElement("div")
+    crearDiv.innerHTML = inputMostrar.value
+    contieneHijo.appendChild(crearDiv)
+
+})
+
+
+
+
+
+
+
+
+
 
 
